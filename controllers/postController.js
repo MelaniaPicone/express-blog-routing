811af9ddfba166f89c722be421fd896a9e439854
MelaniaPicone <melania_picone@yahoo.it>
2025-08-04 18:44:36 +0200

    [add] postControllers

---
+++ controllers/postController.js
@@ -0,0 +1,49 @@
+// importo l'array dei posts
+
+
+// index
+const index = (req, res) => {
+
+
+};
+
+// show
+const show = (req, res) => {
+
+
+};
+
+// store
+const store = (req, res) => {
+
+
+};
+
+// update
+const update = (req, res) => {
+
+
+};
+
+// modify
+const modify = (req, res) => {
+
+
+};
+
+// destroy
+const destroy = (req, res) => {
+
+  
+};
+
+// esporto
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  modify,
+  destroy
+}
