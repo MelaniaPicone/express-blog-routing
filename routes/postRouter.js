8197a2a2eb794ded92775f9b0a60e92bd11df30f
MelaniaPicone <melania_picone@yahoo.it>
2025-08-04 18:04:15 +0200

    [add] params, update, modify

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -14,6 +14,25 @@ router.get('/', (req, res) =>
 
 // show
 router.get('/:id', (req,res) => 
-  {
+  { const id = req.params.id;
 res.send(`Dettaglio del post con id ${id}`);
 });
+
+// store
+router.post('/', (req,res) =>
+{ const id = req.params.id;
+res.send('Inserimento nuovo post');
+});
+
+// update (modifica totale)
+router.put('/:id', (req, res) =>
+{ const id = req.params.id;
+res.send(`Modifica del post con id ${id}`)
+});
+
+// modify (modifica parziale)
+router.patch('/:id', (req, res) =>
+{ const id = req.params.id;
+res.send(`Modifica del post con id ${id}`)
+});
+
