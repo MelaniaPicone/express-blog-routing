2baa2be74914c29be3861962ad62fa2a5396816b
MelaniaPicone <melania_picone@yahoo.it>
2025-08-07 09:55:56 +0200

    [add] corrections, notfound, errorshandler

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const port = 3000;
 // importo postRouter
 const postsRouter = require('./routes/postRouter');
 
+// importo middleware notFound
+const notFound = require('./middlewares/notFound.js');
+
+// importo middleware errorsHandler
+const errorsHandler = require('./middlewares/errorsHandler.js');
+
 // File statici
 app.use(express.static('public'));
 
@@ -24,6 +30,12 @@ app.get('/', (req, res) =>
 {
   res.send("Il mio blog");
 });
+
+// uso middleware notFound
+app.use(notFound);
+
+// uso middleware errorsHandler
+app.use(errorsHandler);
 
 // Il server rimane in ascolto
 app.listen(port,() => console.log(`Server in ascolto sulla porta ${port}`));
